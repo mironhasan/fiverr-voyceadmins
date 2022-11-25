4b58782632be5eaac1784f4ac35b9513b3d457db
mironhasan <mironmahmud018@gmail.com>
2022-11-25 20:21:38 +0600

    Voyce Admins - v1.3

---
+++ vendor/slickslider/slick-call.js
@@ -0,0 +1,41 @@
+// FOR PRICE CARD SLIDER
+$('.price-slider').slick({
+    dots: false,
+    infinite: true,
+    autoplay: false,
+    arrows: true,
+    fade: false,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<i class="fas fa-arrow-right dandik"></i>',
+    nextArrow: '<i class="fas fa-arrow-left bamdik"></i>',
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
+
+$('a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+    $('.price-slider').slick('setPosition');
+});
+
